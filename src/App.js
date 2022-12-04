5cbcc464c9f192fddbfe2702854f6d258de096fb
yonaswoldemichael <ybyonas@gmail.com>
2022-12-04 16:00:44 -0500

    learning

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import './App.css';
 
 const App = () => {
-  const name = 'Bobby';
-  const isNameShowing = false;
+  // const name = null;
+  const name = 'jane';
+  // const isNameShowing = true;
 
 
   return (
     <div className="App">
-  <h1>Hello, {isNameShowing ? name : 'Someone'}!</h1>
+  {/* <h1>Hello, {isNameShowing ? name : 'Someone'}!</h1> */}
+  <h1>Hello, {2 + 2}!</h1>
+  {name ? (
+    <>
+    {/* test */}
+    <h1>{name}</h1>
+    </>
+  ) : (
+    // test
+    <>
+    <h1>test 4</h1>
+    <h2> There is no name</h2>
+    </>
+  )}
     </div>
   );
 }
