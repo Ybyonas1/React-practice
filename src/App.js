a9fed5b23bcb2a6c060bdf02e605ea9daf77de2f
yonaswoldemichael <ybyonas@gmail.com>
2022-12-04 16:09:52 -0500

    props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-const Person =() => {
+const Person =(props) => {
 return (
   <>
-  <h1>Name: John</h1>
-  <h2>Last Name: Doe</h2>
-  <h2>Age: 30</h2>
+  <h1>name: {props.name}</h1>
+  <h2>Last Name: {props.LastName}</h2>
+  <h2>age: {props.age} </h2>
   </>
 )
 }
@@ -14,11 +14,11 @@ const App = () => {
 
   return (
     <div className="App">
- <Person/>
- <Person/>
- <Person/>
- <Person/>
- 
+ <Person name={'John'} LastName={'Doe'} age={25}/>
+ <Person name="Jane" age={2 + 2} LastName="Hendrix" />
+ <Person name={'JBobby'} LastName={'Doesit'} age={35}/>
+ <Person name="Malard" age={2 + 90} LastName="Jenkins" />
+
      </div>
   );
 }
