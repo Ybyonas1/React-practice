96a24993c5206b5283c60cc86b3bc2168fda3091
yonaswoldemichael <ybyonas@gmail.com>
2022-12-04 16:04:45 -0500

    components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import './App.css';
 
-const App = () => {
-  // const name = null;
-  const name = 'jane';
-  // const isNameShowing = true;
+const Person =() => {
+return (
+  <>
+  <h1>Name: John</h1>
+  <h2>Last Name: Doe</h2>
+  <h2>Age: 30</h2>
+  </>
+)
+}
 
+const App = () => {
 
   return (
     <div className="App">
-  {/* <h1>Hello, {isNameShowing ? name : 'Someone'}!</h1> */}
-  <h1>Hello, {2 + 2}!</h1>
-  {name ? (
-    <>
-    {/* test */}
-    <h1>{name}</h1>
-    </>
-  ) : (
-    // test
-    <>
-    <h1>test 4</h1>
-    <h2> There is no name</h2>
-    </>
-  )}
-    </div>
+ <Person/>
+ <Person/>
+ <Person/>
+ <Person/>
+ 
+     </div>
   );
 }
 
