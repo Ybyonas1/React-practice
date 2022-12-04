018d6aa837ad9767076efb034c4ff7ff6bb41b82
yonaswoldemichael <ybyonas@gmail.com>
2022-12-04 16:21:27 -0500

    Using hooks. useState, events, and creating dynamic app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
-const Person =(props) => {
-return (
-  <>
-  <h1>name: {props.name}</h1>
-  <h2>Last Name: {props.LastName}</h2>
-  <h2>age: {props.age} </h2>
-  </>
-)
-}
+
 
 const App = () => {
+  const [counter, setCounter] = useState(0);
 
   return (
     <div className="App">
- <Person name={'John'} LastName={'Doe'} age={25}/>
- <Person name="Jane" age={2 + 2} LastName="Hendrix" />
- <Person name={'JBobby'} LastName={'Doesit'} age={35}/>
- <Person name="Malard" age={2 + 90} LastName="Jenkins" />
-
+<button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
+<h1>{counter}</h1>
+<button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
      </div>
   );
 }
