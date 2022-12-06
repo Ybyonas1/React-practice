ead6ed9c107c38c51521f1b5c851d2c8b36bfd88
yonaswoldemichael <ybyonas@gmail.com>
2022-12-06 15:14:31 -0500

    styling, search bar, and first section added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+import { useEffect } from "react";
 
+import './app.css';
+import SearchIcon from './search.svg';
 
+// 132884e7
+
+const API_URL ='http://www.omdbapi.com?apikey=d87511fd';
+
+const movie1 =
+    {
+        "Title": "Superman Returns",
+        "Year": "2006",
+        "imdbID": "tt0348150",
+        "Type": "movie",
+        "Poster": "https://m.media-amazon.com/images/M/MV5BNzY2ZDQ2MTctYzlhOC00MWJhLTgxMmItMDgzNDQwMDdhOWI2XkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_SX300.jpg"
+    }
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    alert("You've changed the counter to " + counter)
-  // setCounter(100);
-  }, [counter]);
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
 
-  return (
-    <div className="App">
-<button onClick={() => setCounter((prevCount) => prevCount -1)}>-</button>
-<h1>{counter}</h1>
-<button onClick={() => setCounter((prevCount) => prevCount +1)}>+</button>
-     </div>
-  );
+        console.log(data.Search);
+    }
+
+useEffect(() => {
+    searchMovies('Superman')
+}, [])
+ 
+   return (
+    <div className="app">
+        <h1>MovieLand</h1>
+
+        <div className="search">
+            <input 
+            placeholder="Search for movies" 
+            value="Superman" 
+            onChange={() => {}}
+            />
+            <img
+            src={SearchIcon}
+            alt="Search"
+            onClick={() => {}}
+            />
+        </div>
+
+        <div className="container">
+            <div className="movie">
+                <div>
+                    <p>{movie1.Year}</p>
+                </div>
+                <div>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.title}/>
+                </div>
+                <div>
+                    <span>{movie1.Type}</span>
+                    <h3>{movie1.Title}</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+   );
+
 }
 
 export default App;
